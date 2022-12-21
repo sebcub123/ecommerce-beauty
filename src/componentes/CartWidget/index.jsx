22940a4f3f1b6073f1ef6e9fb017711be31c8abe
Sebastian Cubilla <sebcub123@gmail.com>
2022-12-21 19:51:52 -0300

    instale react-icons para el cart terminando el trabajo

--- src/componentes/CartWidget/index.jsx
+++ src/componentes/CartWidget/index.jsx
@@ -4,7 +4,7 @@ const CartWidget = () => {
     return (
         <div>
             <MdShoppingCart style={{ height: 30, width: 30, margin: 10 }} />
-            <span>4</span>
+            <span>3</span>
         </div>
     )
 }
